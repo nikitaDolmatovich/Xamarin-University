55561579323b54ef4ca07626cb6186be281e7df5
nikitaDolmatovich <dolmatovichnik@gmail.com>
2017-08-17 19:14:51 +0300

    Created user interface from code commit

--- TipCalculator/TipCalculator/MyViewController.cs
+++ TipCalculator/TipCalculator/MyViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UIKit;
+using CoreGraphics;
 
 namespace TipCalculator
 {
@@ -7,6 +8,33 @@ namespace TipCalculator
     {
         public MyViewController()
         {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            this.View.BackgroundColor = UIColor.Yellow;
+            UITextField totalAmount = new UITextField(new CGRect(28, 28, View.Bounds.Width - 40, 35));
+            totalAmount.KeyboardType = UIKeyboardType.DecimalPad;
+            totalAmount.BorderStyle = UITextBorderStyle.RoundedRect;
+            totalAmount.Placeholder = "Enter total amount";
+
+            UIButton calcButton = new UIButton(UIButtonType.Custom)
+            {
+                Frame = new CGRect(20, 71, View.Bounds.Width - 40, 45),
+                BackgroundColor = UIColor.FromRGB(0, 0.5f, 0),
+            };
+            calcButton.SetTitle("Calculate",UIControlState.Normal);
+
+            UILabel resultLabel = new UILabel(new CGRect(0, 124, View.Bounds.Width, 40))
+            {
+                TextColor = UIColor.Blue,
+                TextAlignment = UITextAlignment.Center,
+                Font = UIFont.SystemFontOfSize(24),
+                Text = "Tip is $0.00"
+            };
+
+            View.AddSubviews(totalAmount, calcButton, resultLabel);
         }
     }
 }
